aa50ce14417a1d41d4bc32f734b707df560f95f1
Samuel Medina <smdoomed@gmail.com>
2021-09-24 15:47:34 -0500

    deploy

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ app.listen(app.get('port'), async () => {
 })
 
 app.get('/', (req, res) => {
-    res.json(process.env.DB_HOST);
+    res.json('.l.');
 })
 
 app.use( require('./routes/quotes.routes'));
