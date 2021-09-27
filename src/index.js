3fd4ef802fb5a28c1f59753d5694abb42eb7dccd
Samuel Medina <smdoomed@gmail.com>
2021-09-27 18:20:38 -0500

    cors

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const app = express();
 const sequelize = require('./db/db.connection');
+const cors = require('cors');
 require('dotenv').config();
 
 // Middlewares Globales
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(cors());
 app.set('port', process.env.PORT || 3000);
+
 
 // Arranque del servidor del server y de la base de datos
 app.listen(app.get('port'), async () => {
--- src/routes/quotes.routes.js
+++ src/routes/quotes.routes.js
@@ -22,7 +22,7 @@ const {
 // Routes
 app.post('/quotes', checkRegistration, checkExistingAuthor, createQuote); //Postea de una frase si los campos son correctos
 app.get('/quotes', readQuotes) // Obtiene todas las frases que hay en nuestro sistema.
-app.get('/quotes/ramdom', getRandomQuote); // Obtiene una frase aleatoria.
+app.get('/quotes/random', getRandomQuote); // Obtiene una frase aleatoria.
 app.get('/quotes/:id', getOneQuote); // Obtiene una frase de acuerdo al id.
 app.put('/quotes/:id', checkExistingAuthor, postQuote); // Modifica una frase.
 app.delete('/quotes/:id', quoteExists, deleteQuote); // Elimina una frase.
